2cd273d451873386f36fc93be4c4cc52a980d239
Christian Schlake <tisse@schlake.eu>
2016-11-05 17:56:45 +0100

    Update conf_Series.php

--- conf_Series.php
+++ conf_Series.php
@@ -4,27 +4,39 @@
 	*/
 	$confSeries=array(
 		"2-Broke-Girls",
-		"testSerie"
-		);
+		"Two-and-a-Half-Men",
+		"How-I-Met-Your-Mother",
+		"Die-Simpsons",
+		"The-Big-Bang-Theory"
+	);
 	/*
 	ID der Sereie von theTVdb.com
 	*/
 	$confSeriesID=array(
 		"248741",
-		""
+		"72227",
+		"75760",
+		"71663",
+		"80379"
 	);
 	/*
 	Name der Serie im Verzeichnis und prefix der neuen Datei.
 	*/
 	$confSeriesFileName=array(
 		"2_Broke_Girls",
-		""
+		"two_and_a_half_men",
+		"how_I_met_your_mother",
+		"die_simpsons",
+		"the_big_bang_theory"
 	);
 	/*
 	Name des Serienverzeichnis
 	*/
 	$confSeriesDirectory=array(
 		"serien/2_Broke_Girls_(2011)/",
-		""
+		"serien/Two_and_a_Half_Men_(2003)/",
+		"serien/How_I_Met_Your_Mother_(2005)/",
+		"serien/Die_Simpsons_(1989)/",
+		"serien/The_Big_Bang_Theory_(2007)/"
 	);		
 ?>
